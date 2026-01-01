cdec375ac27781e8f5da60b47b9ba54b35e7ccfb
Lucas Malagoli <lucas.malagoli@campit.com.br>
2025-12-31 23:32:37 -0300

    fix: use os envs

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,11 @@
+declare global {
+  interface Window {
+    _env_?: {
+      VITE_API_URL?: string
+    }
+  }
+}
+
 export const config = {
-  API_URL: import.meta.env.VITE_API_URL || "http://localhost:8000",
+  API_URL: window._env_?.VITE_API_URL || import.meta.env.VITE_API_URL || "http://localhost:8000",
 }
